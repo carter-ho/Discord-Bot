4126716a24c551f27ab28dbf258140cfed730b7c
Carter <carterho1103@tamu.edu>
2024-06-15 20:33:14 -0500

    latest update

--- bot.py
+++ bot.py
@@ -10,18 +10,18 @@ from PIL import ImageFont
 
 import constants
 
-
 discord_key = constants.discord_key
-
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+
 
 @bot.command(pass_context=False)
 async def commandhelp(ctx):
@@ -61,6 +61,7 @@ async def commandhelp(ctx):
     Bot sends the message Y"""
     await ctx.send(text)
 
+
 # Hello Command
 @bot.command(pass_context=True)
 async def hello(ctx):
@@ -68,11 +69,13 @@ async def hello(ctx):
     msg = 'Hello {0.author.mention}'.format(ctx.message)
     await ctx.send(msg)
 
+
 # Daily Useless Fact
 @bot.command(pass_context=False)
 async def dailyuseless(ctx):
     uselessFact = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/today")
     await ctx.send(uselessFact.json()["text"])
+
 
 # Random Useless Fact
 @bot.command(pass_context=False)
@@ -80,10 +83,12 @@ async def randomuseless(ctx):
     uselessFact = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
     await ctx.send(uselessFact.json()["text"])
 
+
 @bot.command(pass_context=False)
 async def randomcat(ctx):
     randomCat = requests.get("https://api.thecatapi.com/v1/images/search")
     await ctx.send(randomCat.json()[0]["url"])
+
 
 @bot.command(pass_context=False)
 async def randomdog(ctx):
@@ -92,56 +97,85 @@ async def randomdog(ctx):
 
 
 # Testing function
-@bot.command(pass_context = True) #passing context
-async def salute(ctx): #context gets passed into the first parameter
+@bot.command(pass_context=True)  # passing context
+async def salute(ctx):  # context gets passed into the first parameter
     print(str(ctx.message.author))
     print(str(ctx.message.channel))
     print(str(ctx.message.content))
 
-# 
-@bot.command(pass_contex = True)
+
+#
+@bot.command(pass_contex=True)
 async def helpme(ctx, *, arg):
     text = arg + " Kit!"
-    randomNum = random.randint(0, 1)
+    randomNum = random.randint(1, 100)
     img = ""
-    if(randomNum):
-        img = Image.open('images/wish.png')
+    if (randomNum <= 48):
+        img = Image.open('./images/wish.png')
+    elif randomNum <= 98:
+        img = Image.open('./images/wish2.png')
     else:
-        img = Image.open('images/wish2.png')
-    
+        img = Image.open('./images/wishCard.png')
     I1 = ImageDraw.Draw(img)
-    myFont = ImageFont.truetype("fonts/trebuc.ttf", 110)
+    myFont = ImageFont.truetype("./fonts/trebuc.ttf", 110)
     w, h = img.size
 
-    I1.text((w//2, h * 9/10 ), text, anchor = 'md', font=myFont, fill=(255,255,255), stroke_width=10, stroke_fill=(0,0,0))
+    I1.text((w // 2, h * 9 / 10), text, anchor='md', font=myFont, fill=(255, 255, 255), stroke_width=10,
+            stroke_fill=(0, 0, 0))
 
-    location = "images/wishMeme.png"
+    location = "./images/wishMeme.png"
     img.save(location)
     picture = discord.File(location)
     await ctx.send(file=picture)
     img.close()
-    if(os.path.exists(location)):
+    if (os.path.exists(location)):
         os.remove(location)
 
-@bot.command(pass_contex = False)
+
+@bot.command(pass_contex=True)
+async def clown(ctx, *, arg):
+    text = arg
+    img = Image.open('./images/clown.png')
+    I1 = ImageDraw.Draw(img)
+    myFont = ImageFont.truetype("./fonts/CoreSerifN55Bold.ttf", 50)
+    w, h = img.size
+    I1.text((w // 2, 410), text, anchor='md', font=myFont, fill=(0, 0, 0))
+    location = "./images/clownEdit.png"
+    img.save(location)
+    picture = discord.File(location)
+    await ctx.send(file=picture)
+    img.close()
+    if (os.path.exists(location)):
+        os.remove(location)
+
+@bot.command(pass_context=True)
+async def why(ctx):
+    location = "./gifs/why.gif"
+    gif = discord.File(location)
+    await ctx.send(file=gif)
+
+
+@bot.command(pass_contex=False)
 async def artofwar(ctx):
     f = open("./jsons/artofwar.json")
     data = json.load(f)
     upper = len(data)
-    randomNum = random.randint(0, upper-1)
+    randomNum = random.randint(0, upper - 1)
     await ctx.send(data[randomNum])
 
-@bot.command(pass_context = False)
+
+@bot.command(pass_context=False)
 async def magic8ball(ctx):
     f = open("./jsons/8ball.json")
     data = json.load(f)
     numResponseTypes = len(data)
-    responseType = random.randint(0, numResponseTypes-1)
+    responseType = random.randint(0, numResponseTypes - 1)
     numResponses = len(data[responseType])
-    responseNum = random.randint(0, numResponses-1)
+    responseNum = random.randint(0, numResponses - 1)
     await ctx.send(data[responseType][responseNum])
 
-@bot.command(pass_contex = False)
+
+@bot.command(pass_contex=False)
 async def dice(ctx, arg):
     try:
         arg = int(arg)
@@ -151,9 +185,11 @@ async def dice(ctx, arg):
     else:
         await ctx.send(rand)
 
-@bot.command(pass_contex = True)
+
+@bot.command(pass_contex=True)
 async def copy(ctx, *, arg):
     await ctx.send(arg)
 
-            
+
+
 bot.run(discord_key)
